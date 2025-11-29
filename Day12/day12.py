04a80c8da23c89f48f8a08e200e1c1778098e1f3
FlorianxK <Florian.Koehler@jokonet.de>
2025-11-29 15:41:22 +0100

    day12 part3 works but slow

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,5 +1,5 @@
 from collections import deque
-import heapq
+from copy import deepcopy
 from typing import *
 
 def dayTwelve():
@@ -57,57 +57,43 @@ def dayTwelve2():
 def dayTwelve3():
     #read
     arr = []
-    h = []
-    i = 0
-    with open("Day12/12.txt", 'r') as file:
+    with open("Day12/12_3.txt", 'r') as file:
         for line in file:
             line = line.rstrip()
             arr.append(list(line))
-            for j in range(len(line)):
-                v = int(line[j])
-                heapq.heappush(h, (-v,(i,j)) )
-            i += 1
     m,n = len(arr),len(arr[0])
-    seen = set()
 
-    def explode(i,j):
-        res = 1
-        seen.add( (i,j) )
+    def explode(i,j,seen):
+        nextSeen = deepcopy(seen)
         q = deque([(i,j)])
+
         while q:
             x,y = q.popleft()
-            for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                nx,ny = x+dx,y+dy
-                #false
-                if nx < 0 or nx >= m or ny < 0 or ny >= n or (nx,ny) in seen or int(arr[nx][ny]) > int(arr[x][y]): continue
+            if 0<=x<=m and 0<=y<=n:
+                if (x,y) in nextSeen: continue
+                nextSeen.add( (x,y) )
+                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nx,ny = x+dx,y+dy
+                    if 0<=nx<m and 0<=ny<n and int(arr[nx][ny]) <= int(arr[x][y]):
+                        q.append( (nx,ny) )
+        return nextSeen
 
-                seen.add( (nx,ny) )
-                q.append( (nx,ny) )
-                res += 1
-        return res
-
-    full = 0
-
-    for _ in range(3):
-        temp = h.copy()
-        maxRes = 0
-        el = None
-        while True:
-            v,cord = heapq.heappop(temp)
-            res = explode(*cord)
-            if res < maxRes:
-                break
-            else:
-                maxRes = res
-                el = (v,cord)
-        h.remove(el)
-        full += maxRes
-    return full
+    seen = set()
+    for t in range(3):
+        best = None
+        for i in range(m):
+            for j in range(n):
+                seen1 = explode(i,j,seen)
+                if best is None or len(seen1) > len(best[0]):
+                    best = (seen1,(i,j))
+        seen |= best[0]
+        #print(t, best[1],len(seen))
+    return len(seen)
 
 def main():
     print("Hallo")
-    #print(dayTwelve(), "ist die Lösung von Teil 1")
-    #print(dayTwelve2(), "ist die Lösung von Teil 2")
+    print(dayTwelve(), "ist die Lösung von Teil 1")
+    print(dayTwelve2(), "ist die Lösung von Teil 2")
     print(dayTwelve3(), "ist die Lösung von Teil 3")
 
 if __name__=="__main__":
