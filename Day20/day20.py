ce8f184bf6328be19293c4f413a9b3dc5652c69f
FlorianxK <Florian.Koehler@jokonet.de>
2025-12-11 17:58:39 +0100

    day20 finished

--- Day20/day20.py
+++ Day20/day20.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import defaultdict, deque
 from typing import *
 
 def dayTwenty():
@@ -114,7 +114,7 @@ def dayTwenty3():
     board1 = []
     start = ()
     i = 0
-    with open("Day20/20.txt", 'r') as file:
+    with open("Day20/20_3.txt", 'r') as file:
         for line in file:
             board1.append(list(line.rstrip()))
             for j in range(len(line)):
@@ -157,132 +157,47 @@ def dayTwenty3():
 
     board2 = board120(board1)
     board3 = board120(board2)
+    grids = [board1,board2,board3]
 
     top = [(-1,0),(0,-1),(0,1)]
     bot = [(1,0),(0,-1),(0,1)]
 
-    moves12 = set()
-    moves23 = set()
-    moves31 = set()
-
-    def neighbour(i,j,board):
-        legal = ['T','E','S']
-        res = []
+    def neighbour(i,j):
         # + if True bot and - if False top
         if (i + j) & 1 == 1:
             coord = bot
         else:
             coord = top
-        
+
+        res = []
         for dx,dy in coord:
-            nx,ny = dx+i,dy+j
-            if 0<=nx<m and 0<=ny<n:
-                if board[nx][ny] in legal:
-                    res.append((nx,ny))
+            nx,ny = i+dx,j+dy
+            if 0 <= nx < m and nx <= ny <= n-1-nx:
+                res.append((nx, ny))
         return res
 
-    def verify(i,j,b1,b2,switch):
-        v = 0
-        if b1 == 1 and b2 == 2:
-            pBoard1,pBoard2 = board1,board2
-            v = 1
-        if b1 == 2 and b2 == 3:
-            pBoard1,pBoard2 = board2,board3
-            v = 2
-        if b1 == 3 and b2 == 1:
-            pBoard1,pBoard2 = board3,board1
-            v = 3
+    def neighbours3(i, j, s):
+        return [(nx, dy, (s+1)%3) for nx, dy in ([(i, j)] + neighbour(i, j))]
 
-        if pBoard1[i][j] in legal:
-            if switch == -1:
-                coord = top
-            else:
-                coord = bot
-            for dx,dy in coord:
-                nx,ny = dx+i,dy+j
-                if 0<=nx<m and 0<=ny<n:
-                    if pBoard2[nx][ny] in legal:
-                        a = (i,j)
-                        b = (nx,ny)
-                        t = (min(a,b),max(a,b))
-                        if v == 1:
-                            if t not in moves12:
-                                moves12.add( t )
-                        elif v == 2:
-                            if t not in moves23:
-                                moves23.add( t )
-                        elif v == 3:
-                            if t not in moves31:
-                                moves31.add( t )        
+    def bfs(start):
+        dist = defaultdict(lambda: float("inf"), {start: 0})
+        todo = [start]
+        for i, j, s in todo:
+            if grids[s][i][j] == "E":
+                return dist[i, j, s]
+            for rn, cn, sn in neighbours3(i, j, s):
+                if grids[sn][rn][cn] in "TSE" and dist[rn, cn, sn] == float("inf"):
+                    dist[rn, cn, sn] = dist[i, j, s] + 1
+                    todo.append((rn, cn, sn))
+        return float("inf")
 
-    #pairs between 1 and 2, 2 and 3, 3 and 1 for every move and bfs on pairs
-    legal = ['T','E','S']
-    for i in range(m):
-        switch = -1
-        for j in range(n):
-            verify(i,j,1,2,switch)
-            verify(i,j,2,3,switch)
-            verify(i,j,3,1,switch)
-            if board1[i][j] != '.':
-                switch *= -1
-
-    path = []
-    def bfs():
-        seen = set()
-        seen.add(start)
-        q = deque([(start,0,0)])
-        #currRot 0,1,2%3
-        # CURR ROT IN DEQUE DURCHGEBEN WEIL JEDE MUSS SELBER GUCKEN
-        while True:
-            nextQ = deque([])
-            while q:
-                coord,v,currRot = q.popleft()
-                i,j = coord
-                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                    nx,ny = dx+i,dy+j
-                    #legal
-                    if 0<=nx<m and 0<=ny<n:
-                        a = (i,j)
-                        b = (nx,ny)
-                        t = (min(a,b),max(a,b))
-                        if currRot == 0:
-                            if board2[nx][ny] == 'E' and t in moves12:
-                                return v+1
-                            elif board2[nx][ny] == 'T' and (nx,ny) not in seen:
-                                if t in moves12:
-                                    seen.add( (nx,ny) )
-                                    path.append( ((nx,ny),(currRot+1)%3) )
-                                    nextQ.append( (b,v+1,(currRot+1)%3) )
-
-                        elif currRot == 1:
-                            if board3[nx][ny] == 'E' and t in moves23:
-                                return v+1
-                            elif board3[nx][ny] == 'T' and (nx,ny) not in seen:
-                                if t in moves23:
-                                    seen.add( (nx,ny) )
-                                    path.append( ((nx,ny),(currRot+1)%3) )
-                                    nextQ.append( (b,v+1,(currRot+1)%3) )
-
-                        if currRot == 2:
-                            if board1[nx][ny] == 'E' and t in moves31:
-                                return v+1
-                            elif board1[nx][ny] == 'T' and (nx,ny) not in seen:
-                                if t in moves31:
-                                    seen.add( (nx,ny) )
-                                    path.append( ((nx,ny),(currRot+1)%3) )
-                                    nextQ.append( (b,v+1,(currRot+1)%3) )
-            q = nextQ
-            if not q:
-                print(path)
-                return -1
-
-    res = bfs()
+    res = bfs( (*start,0) )
     return res
 
 def main():
     print("Hallo")
-    #print(dayTwenty(), "ist die Lösung von Teil 1")
-    #print(dayTwenty2(), "ist die Lösung von Teil 2")
+    print(dayTwenty(), "ist die Lösung von Teil 1")
+    print(dayTwenty2(), "ist die Lösung von Teil 2")
     print(dayTwenty3(), "ist die Lösung von Teil 3")
 
 if __name__=="__main__":
